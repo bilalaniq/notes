2d2b17731ccce77d552d0c5d317a1f6acf023495
bilalaniq <bilalaniq2@gmail.com>
2024-11-28 13:58:52 +0500

    updated

--- cpp_cource/vector_class.cpp
+++ cpp_cource/vector_class.cpp
@@ -1,51 +1,64 @@
-#include<iostream>
-#include<initializer_list>
+#include <iostream>
+#include <initializer_list>
 
-template<typename VECTOR>
+template <typename VECTOR, typename T = typename VECTOR::valueType>
 class VectorIterator
 {
-    using valueType = typename VECTOR::valueType;
-    using PointerType = valueType*;
-    using ReferenceType = valueType&;
+    using valueType = T;
+    using PointerType = valueType *;
+    using ReferenceType = valueType &;
+    using ConstPointerType = const valueType *;
+    using ConstReferenceType = const valueType &;
 
 public:
     VectorIterator(PointerType ptr)
-        :m_ptr(ptr) {}
+        : m_ptr(ptr) {}
 
-    VectorIterator& operator++()
+    VectorIterator(ConstPointerType ptr)
+        : m_ptr(ptr) {}
+
+    VectorIterator &operator++()
     {
         m_ptr++;
-        return *this;      //prefix increment operator 
+        return *this;
     }
 
-    VectorIterator operator++(int)   //postfix increment operator
+    VectorIterator operator++(int)
     {
         VectorIterator iterator = *this;
         ++(*this);
-        return iterator;      
+        return iterator;
     }
 
-    VectorIterator& operator--()
+    VectorIterator &operator--()
     {
         m_ptr--;
-        return *this;      //prefix decriment operator 
+        return *this;
     }
 
-    VectorIterator operator--(int)   //postfix decriment operator
+    VectorIterator operator--(int)
     {
         VectorIterator iterator = *this;
         --(*this);
         return iterator;
     }
 
-    ReferenceType operator[](int index)   //postfix decriment operator
+    ReferenceType operator[](int index)
     {
-        return VectorIterator *(m_ptr + index);
-        //or you could do it like this
-        //return VectorIterator * (m_ptr[index]);
+        return *(m_ptr + index);
+    }
+
+    ConstReferenceType operator[](int index) const
+    {
+        return *(m_ptr + index);
     }
 
     PointerType operator->()
+    {
+        return m_ptr;
+    }
+
+    ConstPointerType operator->() const
     {
         return m_ptr;
     }
@@ -55,30 +68,32 @@ public:
         return *m_ptr;
     }
 
-    bool operator==(const VectorIterator& other) const
+    ConstReferenceType operator*() const
+    {
+        return *m_ptr;
+    }
+
+    bool operator==(const VectorIterator &other) const
     {
         return m_ptr == other.m_ptr;
     }
 
-    bool operator!=(const VectorIterator& other) const
+    bool operator!=(const VectorIterator &other) const
     {
         return !(*this == other);
-        //or by using 
-        //return m_ptr != other.m_ptr;
     }
 
 private:
     PointerType m_ptr;
 };
 
-
-
-template<typename T>
+template <typename T>
 class VECTOR
 {
 public:
     using valueType = T;
     using Iterator = VectorIterator<VECTOR<T>>;
+
 public:
     VECTOR()
     {
@@ -90,78 +105,111 @@ public:
         Realloc(size);
     }
 
-    VECTOR(std::initializer_list<T> const& init_list) {
+    VECTOR(std::initializer_list<T> const &init_list)
+    {
         Realloc(init_list.size());
-        //m_Size = init_list.size();
+        // m_Size = init_list.size();
 
         // Copy or move elements from the initializer list to the container
-        for (const auto& value : init_list) {
-            PushBack(value);  // Calls PushBack(const T& value)
+        for (const auto &value : init_list)
+        {
+            PushBack(value); // Calls PushBack(const T& value)
         }
     }
 
-    ~VECTOR() {
+    ~VECTOR()
+    {
         Clear();
         ::operator delete(m_Data, m_Capacity * sizeof(T));
     }
 
-    void PushBack(const T& value)
+    void PushBack(const T &value)
     {
         if (m_Size >= m_Capacity)
             Realloc(m_Capacity + m_Capacity / 2);
 
-        new(&m_Data[m_Size]) T(value);
-        //m_Data[m_Size] = value;
+        new (&m_Data[m_Size]) T(value);
+        // m_Data[m_Size] = value;
         m_Size++;
     }
 
-    void PushBack(T&& value)
+    void PushBack(T &&value)
     {
         if (m_Size >= m_Capacity)
             Realloc(m_Capacity + m_Capacity / 2);
 
-        new(&m_Data[m_Size]) T(std::move(value));
+        new (&m_Data[m_Size]) T(std::move(value));
 
-        //m_Data[m_Size] = std::move(value);
+        // m_Data[m_Size] = std::move(value);
 
         m_Size++;
     }
 
-    template<typename... Args>
-    T& EmplaceBack(Args&&... args)    //this is an inplace constructor
+    template <typename... Args>
+    T &EmplaceBack(Args &&...args) // this is an inplace constructor
     {
         if (m_Size >= m_Capacity)
             Realloc(m_Capacity + m_Capacity / 2);
 
-        new(&m_Data[m_Size]) T(std::forward<Args>(args)...);
+        new (&m_Data[m_Size]) T(std::forward<Args>(args)...);
 
-        //m_Data[m_Size] = T(std::forward<Args>(args)...);
+        // m_Data[m_Size] = T(std::forward<Args>(args)...);
 
-        //The expression T(std::forward<Args>(args)...) creates a temporary object of type T using the forwarded 
-        // arguments. This temporary object will be constructed first and then assigned to m_Data[m_Size].
-        //When you perform the assignment m_Data[m_Size] = ..., you're invoking the assignment operator of type T. 
-        // This means that the existing object at m_Data[m_Size] will be overwritten by the new temporary object.
-       //so use placement new
+        // The expression T(std::forward<Args>(args)...) creates a temporary object of type T using the forwarded
+        //  arguments. This temporary object will be constructed first and then assigned to m_Data[m_Size].
+        // When you perform the assignment m_Data[m_Size] = ..., you're invoking the assignment operator of type T.
+        //  This means that the existing object at m_Data[m_Size] will be overwritten by the new temporary object.
+        // so use placement new
         return m_Data[m_Size++];
     }
+
+    void reserve(size_t newcapacity)
+    {
+        if (newcapacity > m_Capacity)
+        {
+            Realloc(size);
+        }
+    }
+
+    void resize(size_t newcapacity)
+    {
+        Realloc(newcapacity);
+    }
+
+    void assign(size_t newsize, const T &value)
+    {
+        resize(newsize);
+
+        for (size_t i = 0; i < newsize; i++)
+        {
+            m_Data[i] = value;
+        }
+
+        //   you can also do it like this
+        // by using iterators
+        // std::fill(this->begin() , this->end() , value);
+        // or by using using just size
+        // std::fill( m_Data, m_Data + m_Size value);
+    };
 
     void PopBack()
     {
         if (m_Size > 0)
         {
             m_Size--;
-            m_Data[m_Size].~T();    //this cause the double delesion this is why i have used placement new instead of new expression
+            m_Data[m_Size].~T(); // this cause the double delesion this is why i have used placement new instead of new expression
         }
     }
 
-    void Clear() {
+    void Clear()
+    {
         for (size_t i = 0; i < m_Size; i++)
             m_Data[i].~T();
         m_Size = 0;
     }
 
-    const T& operator[](size_t index) const { return m_Data[index]; }
-    T& operator[](size_t index) { return m_Data[index]; }
+    const T &operator[](size_t index) const { return m_Data[index]; }
+    T &operator[](size_t index) { return m_Data[index]; }
 
     size_t Size() const { return m_Size; }
 
@@ -169,26 +217,40 @@ public:
     {
         return Iterator(m_Data);
     }
-
     Iterator end()
     {
         return Iterator(m_Data + m_Size);
     }
 
+    const Iterator cbegin() const
+    {
+        return Iterator(m_Data);
+    }
+
+    const Iterator cend() const
+    {
+        return Iterator(m_Data + m_Size);
+    }
+
+    bool empty() const
+    {
+        return m_Size == 0;
+    }
+
 private:
     void Realloc(size_t newcapacity)
     {
-        //T* newBlock = new T[newcapacity];    we could do the same but this is wrong as it calls the constructors 
-                                               //class that vector has been storing so in future we will be deleting 
-                                            // this block of memory in the future by using the clear function and if the 
-                                           //code end it will then call the distructor of the vector class which will 
-                                          //again call the distructor of the classes and will give us an error 
-        //so we will use new operator instead 
+        // T* newBlock = new T[newcapacity];    we could do the same but this is wrong as it calls the constructors
+        // class that vector has been storing so in future we will be deleting
+        // this block of memory in the future by using the clear function and if the
+        // code end it will then call the distructor of the vector class which will
+        // again call the distructor of the classes and will give us an error
+        // so we will use new operator instead
 
-        T* newBlock = (T*)::operator new(newcapacity * sizeof(T));
+        T *newBlock = (T *)::operator new(newcapacity * sizeof(T));
 
-        //note that this is an operator new not placement new 
-        //note that placemnet new uses new expression not new operator
+        // note that this is an operator new not placement new
+        // note that placemnet new uses new expression not new operator
 
         if (newcapacity < m_Size)
         {
@@ -197,14 +259,13 @@ private:
 
         for (size_t i = 0; i < m_Size; i++)
         {
-            new(&newBlock[i]) T(std::move(m_Data[i]));
+            new (&newBlock[i]) T(std::move(m_Data[i]));
 
-            //newBlock[i] = std::move(m_Data[i]);
+            // newBlock[i] = std::move(m_Data[i]);
 
-            //this is wrong for std::string because the newblock is an place for std::string but is not object itself because we have 
-            //used new operator not new expression it tries to tidy allocate the string which we have in the new block but the current
-            // string never existed because we have allocated memory with out calling the constructor
-            
+            // this is wrong for std::string because the newblock is an place for std::string but is not object itself because we have
+            // used new operator not new expression it tries to tidy allocate the string which we have in the new block but the current
+            //  string never existed because we have allocated memory with out calling the constructor
         }
 
         for (size_t i = 0; i < m_Size; i++)
@@ -218,12 +279,13 @@ private:
     }
 
 private:
-    T* m_Data = nullptr;
+    T *m_Data = nullptr;
     size_t m_Size = 0;
     size_t m_Capacity = 0;
 };
 
-class vector3 {
+class vector3
+{
 public:
     vector3() = default;
 
@@ -239,17 +301,19 @@ public:
         memoryBlock = new int[5];
     }
 
-    ~vector3() {
+    ~vector3()
+    {
         std::cout << "destroyed\n";
         delete[] memoryBlock;
     }
 
-    vector3(const vector3& obj)
-        : x(obj.x), y(obj.y), z(obj.z) {
+    vector3(const vector3 &obj)
+        : x(obj.x), y(obj.y), z(obj.z)
+    {
         std::cout << "copied\n";
     }
 
-    vector3(vector3&& obj) noexcept
+    vector3(vector3 &&obj) noexcept
         : x(obj.x), y(obj.y), z(obj.z)
     {
         obj.x = obj.y = obj.z = 0;
@@ -258,8 +322,10 @@ public:
         std::cout << "moved c\n";
     }
 
-    vector3& operator=(const vector3& obj) {
-        if (this != &obj) {
+    vector3 &operator=(const vector3 &obj)
+    {
+        if (this != &obj)
+        {
             std::cout << "copied\n";
             x = obj.x;
             y = obj.y;
@@ -268,8 +334,10 @@ public:
         return *this;
     }
 
-    vector3& operator=(vector3&& obj) noexcept {
-        if (this != &obj) {
+    vector3 &operator=(vector3 &&obj) noexcept
+    {
+        if (this != &obj)
+        {
             std::cout << "moved a\n";
             x = obj.x;
             y = obj.y;
@@ -281,18 +349,19 @@ public:
         return *this;
     }
 
-    friend std::ostream& operator<<(std::ostream& os, const vector3& vec) {
+    friend std::ostream &operator<<(std::ostream &os, const vector3 &vec)
+    {
         os << "vector3(" << vec.x << ", " << vec.y << ", " << vec.z << ")";
         return os;
     }
 
 private:
     float x{}, y{}, z{};
-    int* memoryBlock = nullptr;      //this is the main problem which have cause us to change all the allocation memory process
-};                               //because of the popback function
+    int *memoryBlock = nullptr; // this is the main problem which have cause us to change all the allocation memory process
+}; // because of the popback function
 
-template<typename T>
-void printvector(const VECTOR<T>& vector)
+template <typename T>
+void printvector(const VECTOR<T> &vector)
 {
     for (size_t i = 0; i < vector.Size(); i++)
     {
@@ -305,29 +374,27 @@ int main()
 {
     {
         VECTOR<std::string> vector;
-        //VECTOR<int> vector;
-        //vector.PushBack(vector3(1.0f));
+        // VECTOR<int> vector;
+        // vector.PushBack(vector3(1.0f));
         std::string a = std::string("bilal");
         vector.EmplaceBack("bilal");
         vector.PushBack(std::string("bilal"));
         vector.PushBack(a);
 
-
         vector.EmplaceBack("bilal");
         vector.EmplaceBack("bilal");
 
-        //vector.PushBack("bilal");
+        // vector.PushBack("bilal");
 
         /*for (VECTOR<int>::Iterator it = vector.begin(); it != vector.end(); ++it)
         {
             std::cout << *it << std::endl;
         }*/
 
-        for(auto& value : vector)
+        for (auto &value : vector)
         {
             std::cout << value << std::endl;
         }
-
 
         /*vector.PushBack(vector3(2, 3, 4));
         vector.PushBack(vector3());*/
